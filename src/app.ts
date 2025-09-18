d9c265c8b7a89ea09ca52ebe626602e439d8d9fb
Rahul Pal <usefulltech1819@gmail.com>
2025-09-18 21:17:02 +0530

    fix bug

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
+import cookiesParser from "cookie-parser";
+import cors from "cors";
 
+dotenv.config();
+const app = express();
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookiesParser());
+
+// cors configrations
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(","),
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+export default app;
