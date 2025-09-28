e13faea51262d8a944387485c416eac23a0faa15
Rahul Pal <usefulltech1819@gmail.com>
2025-09-28 12:30:07 +0530

    Add Database config

---
+++ src/config/db.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+let isConneted = true;
+
+export const connectDB = async () => {
+    if (isConneted) return;
+    try {
+        const db = await mongoose.connect(process.env.DB_URL!);
+        isConneted = db.connection.readyState === 1;
+        console.log("✅ MongoDB connected");
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error("❌ MongoDB error:", error.message);
+        } else {
+            console.error("❌ MongoDB error:", error);
+        }
+        process.exit(1);
+    }
+};
