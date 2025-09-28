46031534f530e813d36339ea629fb1b9593fc377
Rahul Pal <usefulltech1819@gmail.com>
2025-09-28 12:33:47 +0530

    Add server.ts file

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,12 @@
 import app from "./app.js";
-import { connectDB } from "./config/db.js";
-
-app.listen(PORT);
+import { connectDB } from "./config/db.js"
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log("Server start at: " + process.env.PORT);
+        });
+    })
+    .catch((err) => {
+        console.error("MongoDB connection error: ", err);
+        process.exit(1);
+    });
