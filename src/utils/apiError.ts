1bdd7b239bad99186990e11ec9b2c9f1d9a4cec5
Rahul Pal <usefulltech1819@gmail.com>
2025-09-29 08:53:54 +0530

    Add api error handler

---
+++ src/utils/apiError.ts
@@ -0,0 +1,28 @@
+interface Errors {
+    statusCode: number,
+    message: string,
+    success: boolean,
+    errors: any
+}
+
+class ApiError extends Error implements Errors  {
+    statusCode: number;
+    message: string;
+    success: boolean;
+    errors: any;
+    constructor ( statusCode: number, message: string = "Something went wrong", errors: any = [], stack: string = "" ) {
+        super(message)
+        this.statusCode = statusCode;
+        this.message = message;
+        this.success = false;
+        this.errors = errors;
+
+        if (stack) {
+            this.stack = stack;
+        } else {
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+}
+
+export { ApiError };
