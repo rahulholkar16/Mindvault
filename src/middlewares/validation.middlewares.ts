6130890a4f7287e7159e1ad6a11a4490a2dfb727
Rahul Pal <usefulltech1819@gmail.com>
2025-10-24 20:21:51 +0530

    Add validation middleware

---
+++ src/middlewares/validation.middlewares.ts
@@ -0,0 +1,41 @@
+import type { NextFunction, Response, Request } from "express";
+import * as z from "zod";
+
+const UserSchema = z.object({
+    name: z
+        .string({ message: "Invalid name!" })
+        .trim()
+        .min(3, { message: "Name must be at least 3 characters long!" })
+        .max(15, { message: "Name must be at most 15 characters long!" }),
+
+    email: z.preprocess(
+        (val) => (typeof val === "string" ? val.trim().toLowerCase() : val),
+        z.email({ message: "Invalid email address" })
+    ),
+
+    password: z
+        .string()
+        .min(8, { message: "Password must be at least 8 characters long" })
+        .max(64, { message: "Password must be at most 64 characters long" })
+        .regex(/[A-Z]/, {
+            message: "Password must contain at least one uppercase letter",
+        })
+        .regex(/[a-z]/, {
+            message: "Password must contain at least one lowercase letter",
+        })
+        .regex(/[0-9]/, { message: "Password must contain at least one number" })
+        .regex(/[@$!%*?&#]/, {
+            message:
+                "Password must contain at least one special character (@, $, !, %, *, ?, &, #)",
+        }),
+});
+
+const ValidationMiddleware = (req: Request<{}, {}, { name: string; email: string; password: string;  }>, res: Response, next: NextFunction) => {
+    const { name, email, password } = req.body;
+    const result = UserSchema.safeParse({ name, email, password });
+    if (!result.success) {
+        return res.status(400).json({ errors: result.error.format() });
+    }
+    req.validateData = result.data;
+    next();
+};
