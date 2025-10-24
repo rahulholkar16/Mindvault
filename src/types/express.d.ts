7c6363504980bf309e7ac8e7d1d0ec4abaa8622d
Rahul Pal <usefulltech1819@gmail.com>
2025-10-24 20:23:26 +0530

    Add Request type'

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,9 +1,13 @@
 import type { I_UserDocument } from "../models/user.model.ts";
+import type * as z from "zod";
+import type { UserSchema } from "../middlewares/validation.middlewares.ts";
+
 
 declare global {
     namespace Express {
         interface Request {
-            user?: I_UserDocument
+            user?: I_UserDocument;
+            validateData?: z.infer<typeof UserSchema>;
         }
     }
 }
