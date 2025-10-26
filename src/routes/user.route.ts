0d490db11449807c2d10e4c86bbf91ee85300d08
Rahul Pal <usefulltech1819@gmail.com>
2025-10-26 13:00:17 +0530

    Add verify email route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { ValidationMiddleware } from "../middlewares/validation.middlewares.js";
-import { login, register } from "../controllers/auth.controller.js";
+import { getCurrentUser, login, register, verifyEmail } from "../controllers/auth.controller.js";
+import { auth } from "../middlewares/auth.middlewares.js"
 const router = Router();
 
 router.route("/register").post(ValidationMiddleware, register);
 router.route("/login").get(login);
-router.route("/login").delete(login);
-
+router.route("/login").delete(auth, login);
+router.route("/me").get(auth, getCurrentUser);
+router.route("/verify-email/:verificationToken").get(verifyEmail);
 export default router;
