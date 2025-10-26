ec2bbb3a706a0a27b49c030e24815a7a7a90f23d
Rahul Pal <usefulltech1819@gmail.com>
2025-10-26 13:01:31 +0530

    Add resend verify email route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { ValidationMiddleware } from "../middlewares/validation.middlewares.js";
-import { getCurrentUser, login, register, verifyEmail } from "../controllers/auth.controller.js";
+import { getCurrentUser, login, register, verifyEmail, resendEmailVerification } from "../controllers/auth.controller.js";
 import { auth } from "../middlewares/auth.middlewares.js"
 const router = Router();
 
@@ -9,4 +9,5 @@ router.route("/login").get(login);
 router.route("/login").delete(auth, login);
 router.route("/me").get(auth, getCurrentUser);
 router.route("/verify-email/:verificationToken").get(verifyEmail);
+router.route("/resend-email-verification").get(auth, resendEmailVerification);
 export default router;
