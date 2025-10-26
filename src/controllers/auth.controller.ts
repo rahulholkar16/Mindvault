8fee330dd4cad8584fc0895941ce4ba0cb6b7262
Rahul Pal <usefulltech1819@gmail.com>
2025-10-26 11:40:52 +0530

    Add login controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,50 +1,110 @@
 import type { Request, Response } from "express";
-import { UserModel } from "../models/user.model.js"
+import { UserModel } from "../models/user.model.js";
 import { ApiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { emailVerificationContent, sendEmail } from "../services/sendMail.services.js";
+import {
+    emailVerificationContent,
+    sendEmail,
+} from "../services/sendMail.services.js";
 import { ApiResponse } from "../utils/apiResponse.js";
+import type { Types } from "mongoose";
 
-const generateAccessAndRefreshToken = async (userId: string) => {
+const generateAccessAndRefreshToken = async (
+    userId: string | Types.ObjectId
+) => {
     try {
         const user = await UserModel.findById(userId);
-        if(!user) throw new ApiError(404, "User not found");
+        if (!user) throw new ApiError(404, "User not found");
         const accessToken = user.generateAccessToken();
         const refreshToken = user.generateRefreshToken();
         user.refershToken = refreshToken;
         await user.save({ validateBeforeSave: false });
-        return { accessToken, refreshToken }
+        return { accessToken, refreshToken };
     } catch (error) {
-        throw new ApiError(500, "Something went wrong while generating access token.");
+        throw new ApiError(
+            500,
+            "Something went wrong while generating access token."
+        );
     }
 };
 
-export const registerUser = asyncHandler(async (req: Request, res: Response)=>{
+export const register = asyncHandler(async (req: Request, res: Response) => {
     const { name, email, password } = req.validateData;
     const isExist = await UserModel.findOne({ email });
-    if(isExist) throw new ApiError(409, "User already exists.");
+    if (isExist) throw new ApiError(409, "User already exists.");
     const newUser = await UserModel.create({
         name,
         email,
         password,
     });
-    const { unHashedToken, hasedToken, tokenExpiry } = newUser.generateTempToken();
-
+    const { unHashedToken, hasedToken, tokenExpiry } =
+        newUser.generateTempToken();
     newUser.verificationToken = hasedToken;
     newUser.verificationTokenExpire = tokenExpiry;
-
     await sendEmail({
         email: newUser.email,
         subject: "Plesase verify your email.",
-        mailgenContent: emailVerificationContent(newUser.name, `${req.protocol}://${req.get("host")}/api/vi/user/verify-email/${unHashedToken}`)
+        mailgenContent: emailVerificationContent(
+            newUser.name,
+            `${req.protocol}://${req.get(
+                "host"
+            )}/api/vi/user/verify-email/${unHashedToken}`
+        ),
     });
-    console.log(newUser);
-    
     const data = await UserModel.findById(newUser._id).select(
         "-password -verificationToken -resetPasswordToken -refreshToken"
     );
-    if (!data) throw new ApiError(500, "Something went wrong while Register a User.");
-    return res.status(200).json(
-        new ApiResponse(200, { user: data }, "User added successfully and verification email send")
-    )
+
+    if (!data)
+        throw new ApiError(500, "Something went wrong while Register a User.");
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                { user: data },
+                "User added successfully and verification email send"
+            )
+        );
+});
+
+export const login = asyncHandler(async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    if (!email || !password)
+        throw new ApiError(401, "Email and Password required.");
+
+    const user = await UserModel.findOne({ email });
+    if (!user) throw new ApiError(404, "User not found!");
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+    if (!isPasswordValid) throw new ApiError(401, "Invalid Password!");
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+        user._id
+    );
+
+    const loggedUser = await UserModel.findById(user._id).select(
+        "-password -verificationToken -resetPasswordToken -refreshToken"
+    );
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    };
+
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    user: loggedUser,
+                    accessToken,
+                    refreshToken,
+                },
+                "User logged in successfully."
+            )
+        );
 });
