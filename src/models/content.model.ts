0a22fd729d4de9dd36e6c768f629b20d3ccc480e
Rahul Pal <usefulltech1819@gmail.com>
2025-10-30 14:46:44 +0530

    Add Content Interface

--- src/models/content.model.ts
+++ src/models/content.model.ts
@@ -1,12 +1,22 @@
-import mongoose from "mongoose";
+import mongoose, { Types } from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const content = new Schema({
+export interface I_Content extends mongoose.Document {
+    "_id": Types.ObjectId,
+    "title": string,
+    "url"?: string,
+    "tags"?: Types.ObjectId[],
+    "userId": Types.ObjectId,
+    "isPublic"?: boolean
+};
+
+const content = new Schema<I_Content>({
     title: { type: String, required: true },
     url: { type: String },
     tags: [ { type: mongoose.Types.ObjectId, ref: "Tag" } ],
     userId: { type: Schema.Types.ObjectId, ref: "user", required: true },
+    isPublic: { type: Boolean, default: false }
 });
 
 export const ContentModel = mongoose.models?.Content || mongoose.model("Content", content);
