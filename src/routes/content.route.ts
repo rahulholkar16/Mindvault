964a33e0b6b580515d0cea8988d23612181eda42
Rahul Pal <usefulltech1819@gmail.com>
2025-11-12 21:43:01 +0530

    Add get content route

--- src/routes/content.route.ts
+++ src/routes/content.route.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { auth } from "../middlewares/auth.middlewares.js";
-import { createContent } from "../controllers/content.controller.js";
+import { createContent, getAllContent } from "../controllers/content.controller.js";
 
 const router = Router();
 
 router.route("/content").post(auth, createContent);
+router.route('/content').get(auth, getAllContent);
