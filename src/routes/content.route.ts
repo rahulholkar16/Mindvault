6fbe817889cb12e6b7317c89b8194cb256503340
Rahul Pal <usefulltech1819@gmail.com>
2025-11-12 21:44:05 +0530

    Add get content by ID route

--- src/routes/content.route.ts
+++ src/routes/content.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { auth } from "../middlewares/auth.middlewares.js";
-import { createContent, getAllContent } from "../controllers/content.controller.js";
+import { createContent, getAllContent, getContentById } from "../controllers/content.controller.js";
 
 const router = Router();
 
 router.route("/content").post(auth, createContent);
 router.route('/content').get(auth, getAllContent);
+router.route('/content/:contentId').get(auth, getContentById);
