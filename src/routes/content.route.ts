06286d3b7be0696796ce9fd9eb77257807271bd5
Rahul Pal <usefulltech1819@gmail.com>
2025-11-12 21:53:58 +0530

    Add delete Content route

--- src/routes/content.route.ts
+++ src/routes/content.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { auth } from "../middlewares/auth.middlewares.js";
-import { createContent, getAllContent, getContentById } from "../controllers/content.controller.js";
+import { createContent, deleteContent, getAllContent, getContentById } from "../controllers/content.controller.js";
 
 const router = Router();
 
 router.route("/content").post(auth, createContent);
 router.route("/content").get(auth, getAllContent);
 router.route("/content/:contentId").get(auth, getContentById);
+router.route("/content/:contentId").get(auth, getContentById);
+router.route("/content/:contentId").delete(auth, deleteContent);
