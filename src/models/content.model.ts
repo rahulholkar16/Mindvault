d1748989b52fe43a02203a620df69d96f190b351
Rahul Pal <usefulltech1819@gmail.com>
2026-01-26 19:45:12 +0530

    Add Pre hook to convert Youtube video url to embed url

--- src/models/content.model.ts
+++ src/models/content.model.ts
@@ -7,7 +7,7 @@ export interface I_Content extends mongoose.Document {
     "title": string,
     "url"?: string,
     "tags"?: Types.ObjectId[],
-    "type"?: string,
+    "type"?: "document" | "video" | "tweets" | "link",
     "userId": Types.ObjectId,
     "isPublic"?: boolean
 };
@@ -27,6 +27,24 @@ content.pre("deleteOne", async function (this: I_Content, next) {
         { _id: userId },
         { $pull: { content: this._id } }
     );
+    next();
+});
+
+content.pre("save", async function (next) {
+    if (this.type !== "video" || !this.url) return next();
+    
+    const url = this.url; 
+    let videoId: string | undefined;
+
+    if (url.includes("youtu.be")) {
+        videoId = url.split("youtu.be/")[1];
+    } else if (url.includes("v=")) {
+        videoId = url.split("v=")[1]?.split("&")[0];
+    }
+
+    if (!videoId) return next();
+
+    this.url = `https://www.youtube.com/embed/${videoId}`;
     next();
 });
 
