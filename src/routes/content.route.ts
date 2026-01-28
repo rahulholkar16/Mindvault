a307d3a361d599cd17ad26ff7701849ae03d61ad
Rahul Pal <usefulltech1819@gmail.com>
2026-01-28 23:39:51 +0530

    Add route of specific content

--- src/routes/content.route.ts
+++ src/routes/content.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { auth } from "../middlewares/auth.middlewares.js";
-import { createContent, deleteContent, getAllContent, getContentById } from "../controllers/content.controller.js";
+import { createContent, deleteContent, getAllContent, getContentById, getSpecificContent } from "../controllers/content.controller.js";
 
 const router = Router();
 
 router.route("/content").post(auth, createContent);
 router.route("/content").get(auth, getAllContent);
+router.route("/content/:type").get(auth, getSpecificContent);
 router.route("/content/:contentId").get(auth, getContentById);
 router.route("/content/:contentId").get(auth, getContentById);
 router.route("/content/:contentId").delete(auth, deleteContent);
