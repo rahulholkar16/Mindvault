b259d4234fdf50e1abe219535a2358f862921163
Rahul Pal <usefulltech1819@gmail.com>
2026-01-30 02:08:29 +0530

    Add persnol route

--- src/routes/content.route.ts
+++ src/routes/content.route.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { auth } from "../middlewares/auth.middlewares.js";
-import { createContent, deleteContent, getAllContent, getContentById, getSpecificContent } from "../controllers/content.controller.js";
+import { createContent, deleteContent, getAllContent, getAllContentMe, getContentById, getSpecificContent, getSpecificContentMe } from "../controllers/content.controller.js";
 
 const router = Router();
 
 router.route("/content").post(auth, createContent);
 router.route("/content").get(auth, getAllContent);
+router.route("/me/content").get(auth, getAllContentMe);
 router.route("/content/:type").get(auth, getSpecificContent);
+router.route("/me/content/:type").get(auth, getSpecificContentMe);
 router.route("/content/:contentId").get(auth, getContentById);
 router.route("/content/:contentId").get(auth, getContentById);
 router.route("/content/:contentId").delete(auth, deleteContent);
