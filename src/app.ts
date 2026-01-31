2159204cd669feb2c8f4ca9084455124783ec6ef
Rahul Pal <usefulltech1819@gmail.com>
2026-02-01 01:16:19 +0530

    Move config in server.ts

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
 import express from "express";
-import dotenv from "dotenv";
 import cookiesParser from "cookie-parser";
 import cors from "cors";
 import authRouter from "./routes/user.route.js";
 import contentRouter from "./routes/content.route.js";
 import { errorHandler } from "./middlewares/errorrHandler.middlewares.js";
 
-dotenv.config();
+
 const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
