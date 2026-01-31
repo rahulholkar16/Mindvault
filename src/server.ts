61539a422b6d616e50d05b27cad3f0d8b06d75fb
Rahul Pal <usefulltech1819@gmail.com>
2026-02-01 01:16:53 +0530

    Add Dotconfig

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
+import dotenv from "dotenv";
+import "dotenv/config";
 import app from "./app.js";
-import { connectDB } from "./config/db.js"
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
 connectDB()
     .then(() => {
         app.listen(process.env.PORT, () => {
