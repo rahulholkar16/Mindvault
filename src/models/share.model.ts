50388561e781a698623885e3d81d6d3c95a7f0f9
Rahul Pal <usefulltech1819@gmail.com>
2026-02-05 00:39:41 +0530

    Remove Content Id

--- src/models/share.model.ts
+++ src/models/share.model.ts
@@ -3,13 +3,11 @@ import mongoose, { model, Types } from "mongoose";
 const Schema = mongoose.Schema;
 
 export interface I_Share extends mongoose.Document {
-    "contentId": Types.ObjectId;
     "shareToken": string;
     "userId": Types.ObjectId;
 }
 
 const shareSchema = new Schema<I_Share>({
-    contentId: { type: Schema.Types.ObjectId, ref: "Content", required: true },
     shareToken: { type: String, unique: true, sparse: true },
     userId: { type: Schema.Types.ObjectId, ref: "User", required: true }
 }, {
