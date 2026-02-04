197fb3d88dc767995b08053f4c407ce7895beb84
Rahul Pal <usefulltech1819@gmail.com>
2026-02-04 17:58:08 +0530

    Update Share Schema

--- src/models/share.model.ts
+++ src/models/share.model.ts
@@ -3,12 +3,14 @@ import mongoose, { model, models, Types } from "mongoose";
 const Schema = mongoose.Schema;
 
 export interface I_Share extends mongoose.Document {
-    "hash": string;
+    "contentId": Types.ObjectId;
+    "shareToken": string;
     "userId": Types.ObjectId;
 }
 
 const shareSchema = new Schema<I_Share>({
-    hash: { type: String, unique: true, required: true },
+    contentId: { type: Schema.Types.ObjectId, ref: "Content", required: true },
+    shareToken: { type: String, unique: true, sparse: true },
     userId: { type: Schema.Types.ObjectId, ref: "User", required: true }
 }, {
     timestamps: true
