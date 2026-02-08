52fcb8cae83b08deaad870b582cb035dd4198062
Rahul Pal <usefulltech1819@gmail.com>
2026-02-09 02:39:59 +0530

    Change DB

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,33 +1,19 @@
 import mongoose from "mongoose";
 
-mongoose.set("bufferCommands", false); // 🔥 VERY IMPORTANT FOR VERCEL
-
-let isConnected = false;
+let isConneted = false;
 
 export const connectDB = async () => {
-    if (isConnected) {
-        console.log("♻️ Mongo already connected (cached)");
-        return mongoose.connection;   // 👈 ADD THIS
-    }
-
+    if (isConneted) return;
     try {
-        console.log("⏳ Connecting to Mongo...");
-
-        const db = await mongoose.connect(
-            process.env.MONGO_URI ||
-            "mongodb+srv://tech16:Rahul%231819@cluster0.ovgz62a.mongodb.net/SECOUNDBRAIN",
-            {
-                serverSelectionTimeoutMS: 10000,
-                maxPoolSize: 5,
-            }
-        );
-
-        isConnected = db.connection.readyState === 1;
-        console.log("✅ MongoDB connected successfully!");
-        return db.connection;   // 👈 ADD THIS
-    } catch (error: any) {
-        console.error("❌ MongoDB Connection Error:", error.message);
-        throw error;
+        const db = await mongoose.connect(process.env.DB_URL!);
+        isConneted = db.connection.readyState === 1;
+        console.log("✅ MongoDB connected");
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error("❌ MongoDB error:", error.message);
+        } else {
+            console.error("❌ MongoDB error:", error);
+        }
+        process.exit(1);
     }
 };
-
