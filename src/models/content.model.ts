a289c45c998edf164a7427739ab95b8ddd3742b2
Rahul Pal <usefulltech1819@gmail.com>
2026-02-09 02:22:30 +0530

    Change content model

--- src/models/content.model.ts
+++ src/models/content.model.ts
@@ -1,5 +1,5 @@
 import mongoose, { Types } from "mongoose";
-import { UserModel } from "./user.model.js";
+// import { UserModel } from "./user.model.js";
 
 const Schema = mongoose.Schema;
 
@@ -42,13 +42,13 @@ content.pre("save", async function (next) {
     next();
 });
 
-content.post("save", async function () {
-    const contentId = this._id;
-    const userId = this.userId;
-    await UserModel.findByIdAndUpdate(userId, {
-        $push: { content: contentId}
-    });
-});
+// content.post("save", async function () {
+//     const contentId = this._id;
+//     const userId = this.userId;
+//     await UserModel.findByIdAndUpdate(userId, {
+//         $push: { content: contentId}
+//     });
+// });
 
 
 export const ContentModel = (mongoose.models?.Content as mongoose.Model<I_Content>) || (mongoose.model<I_Content>("Content", content));
