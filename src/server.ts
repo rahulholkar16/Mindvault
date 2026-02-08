aa13c5ee23ece364781a17f603f837511b00267c
Rahul Pal <usefulltech1819@gmail.com>
2026-02-08 23:22:29 +0530

    fix  vercel

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 import app from "./app.js";
 import { connectDB } from "./config/db.js";
 
