003576c49c3f15c45c3f604a3f997da7aff40127
Rahul Pal <usefulltech1819@gmail.com>
2026-02-09 01:13:06 +0530

    change server

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,14 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
 import app from "./app.js";
 import { connectDB } from "./config/db.js";
 
-export default async function handler(
-    req: VercelRequest,
-    res: VercelResponse
-) {
-    try {
-        console.log("🔥 Handler started");
-        await connectDB();
-        console.log("✅ Mongo connected inside handler");
-        return app(req, res);
-    } catch (err: any) {
-        return res.status(500).json({
-            success: false,
-            message: "Database connection failed",
-            error: err?.message || "Unknown error",
+connectDB()
+    .then(() => {
+        console.log("✅ Mongo Connected");
+        
+        app.listen(3000, () => {
+            console.log("🚀 Server running");
         });
-    }
-}
+    })
+    .catch((err) => {
+        console.error("❌ DB connection failed", err);
+    });
