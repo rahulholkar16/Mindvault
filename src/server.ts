d814945ba803556f9f05d2ca84d0e0162f3e9319
Rahul Pal <usefulltech1819@gmail.com>
2026-02-09 02:12:58 +0530

    change server

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 import app from "./app.js";
 import { connectDB } from "./config/db.js";
 
-connectDB()
-    .then(() => {
-        console.log("✅ Mongo Connected");
-        
-        app.listen(3000, () => {
-            console.log("🚀 Server running");
-        });
-    })
-    .catch((err) => {
-        console.error("❌ DB connection failed", err);
-    });
+export default async function handler(
+    req: VercelRequest,
+    res: VercelResponse
+) {
+    console.log("🔥 HANDLER STARTED");
+
+    try {
+        await connectDB();
+        return app(req, res);
+    } catch (err: any) {
+        console.error("❌ DB ERROR:", err.message);
+        return res.status(500).json({ message: "Database connection failed" });
+    }
+}
