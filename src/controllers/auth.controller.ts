6c0202b0d94715871675bb6179d573a37a6f784c
Rahul Pal <usefulltech1819@gmail.com>
2026-02-09 13:25:01 +0530

    Update cookie option

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -153,7 +153,9 @@ export const logout = asyncHandler(async (req: Request, res: Response) => {
 
     const options = {
         httpOnly: true,
-        secure: true
+        secure: true,
+        sameSite: "none",
+        path: "/"
     };
 
     return res.status(200)
