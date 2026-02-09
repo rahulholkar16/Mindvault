08d75b6ca498e679acf4cf96af0ec1b6baf50319
Rahul Pal <usefulltech1819@gmail.com>
2026-02-09 12:21:20 +0530

    update backend

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,17 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+import "dotenv/config";
+import app from "./app.js";
+import { connectDB } from "./config/db.js"
 
-export default async function handler(
-    req: VercelRequest,
-    res: VercelResponse
-) {
-    console.log("🔥 HANDLER STARTED");
-
-    try {
-        await connectDB();   // 🔥 PEHLE DB CONNECT
-
-        // 👇 IMPORT APP *AFTER* DB CONNECT (CRUCIAL)
-        const app = (await import("./app.js")).default;
-
-        return app(req, res);
-    } catch (err: any) {
-        console.error("❌ DB ERROR:", err.message);
-        return res.status(500).json({ message: "Database connection failed" });
-    }
-}
+dotenv.config();
+connectDB()
+    .then(() => {
+    
+        app.listen(process.env.PORT, () => {
+            console.log("MINDVAULT start at: " + process.env.PORT);
+        });
+    })
+    .catch((err) => {
+        console.error("MongoDB connection error: ", err);
+        process.exit(1);
+    });
